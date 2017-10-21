a2c32fffa822da0920dcf6c00bd898c16fdca629
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-10-21 23:13:27 +0600

    persistant menu debug

--- setBotProperties.php
+++ setBotProperties.php
@@ -13,7 +13,7 @@
 	  "persistent_menu":[
 						  {
 						    "locale":"default",
-						    "composer_input_disabled": true,
+						    "composer_input_disabled": false,
 						    "call_to_actions":[
 											      {
 											        "title":"My Settings",
